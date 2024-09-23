f9a1161dbc29443c51eda5b5ce4a1f706e0f0d4f
Carlos Miguel Rodriguez Santana <cmrodriguezsantana23gmail.com>
2024-09-23 19:47:13 +0200

    Creacion del header junto con estilos y aplicados estilos globales

---
+++ includes/header.inc.php
@@ -0,0 +1,8 @@
+<header>
+    <nav>
+        <a href="" id="logo">
+            <img src="/icons/logo-social-media.svg" alt="Logo de la red social">
+            <div>Social Media</div>
+        </a>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<?php
+require_once($_SERVER['DOCUMENT_ROOT']. '/includes/head.inc.php');
+?>    
+<body>
+    <?php
+    require_once($_SERVER['DOCUMENT_ROOT']. '/includes/header.inc.php');
+    ?>   
+
+</body>
+</html>
